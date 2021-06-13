7ee03854ad44d8c224192a59686d0554e7d4038e
Keiichi Watanabe <udon.watanabe@gmail.com>
2021-06-13 10:19:30 +0900

    Add simple web interface

--- app/index.ts
+++ app/index.ts
@@ -3,15 +3,72 @@
 // will work here one day as well!
 const rust = import('../public/pkg/index');
 
-function render_board(s: string) {
-    let board = document.getElementById("board");
+const BOARD_ID: string = "board";
+const MONTH_FORM_ID: string = "month-form";
+const DAY_FORM_ID: string = "day-form";
+const FLIP_CHECK_ID: string = "allow-flip-check";
+
+function findSolution() {
+    const m_form =<HTMLSelectElement>document.getElementById(MONTH_FORM_ID);
+    const month = m_form.selectedIndex + 1;
+    const d_form =<HTMLSelectElement>document.getElementById(DAY_FORM_ID);
+    const day = d_form.selectedIndex + 1;
+    const flip_form =<HTMLInputElement>document.getElementById(FLIP_CHECK_ID);
+    const allow_flip = flip_form.checked;
+
+    resetBoard();
+
+    callSolver(month, day, allow_flip).then(board => {
+        renderBoard(board);
+    }).catch(console.error);
+}
+
+function resetBoard() {
+    let board = document.getElementById(BOARD_ID);
+    board.innerText = "Searching...";
+}
+
+function renderBoard(s: string) {
+    let board = document.getElementById(BOARD_ID);
     board.innerText = s;
 }
 
+async function callSolver(month: number, day: number, allow_flip: boolean): Promise<string> {
+    if (!(1 <= month && month <= 12 && 1 <= day && day <= 31)) {
+        console.error("Error: invalid date", month, day);
+        return Promise.reject();
+    }
 
-rust.then(m => {
-    let x = m.solution(0, 0, 1, 0);
-    render_board(x);
-    console.log("Hello!");
-})
-    .catch(console.error);
+    return rust.then(m => {
+        const s: string = m.find_solution(month, day, allow_flip);
+        return s;
+    });
+}
+
+function addOptions() {
+    const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const m_form =<HTMLSelectElement>document.getElementById(MONTH_FORM_ID);
+    months.forEach(m => {
+        const opt = document.createElement("option");
+        opt.text = m;
+        m_form.add(opt);
+    });
+
+    const d_form =<HTMLSelectElement>document.getElementById(DAY_FORM_ID);
+    for (let i = 1; i <= 31; i++) {
+        const opt = document.createElement("option");
+        opt.text = i.toString();
+        d_form.add(opt);
+    }
+}
+
+
+
+function initialize() {
+    document.getElementById("find-button").onclick=findSolution;
+
+    addOptions();
+}
+
+initialize();
--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,40 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(mx: i32, my: i32, dx: i32, dy: i32) {
-    let m = Point::new(mx, my);
-    let d = Point::new(dx, dy);
-    let s = get_solution(m, d);
-    alert(&format!("Hello, World!\n{}", s[0]));
+pub fn greet(m: i32, d: i32) {
+    let s = find_solution(m, d, false);
+    alert(&format!("Hello, World!\n{}", s));
 }
 
 #[wasm_bindgen]
-pub fn solution(mx: i32, my: i32, dx: i32, dy: i32) -> String {
-    let m = Point::new(mx, my);
-    let d = Point::new(dx, dy);
-    get_solution(m, d)[0].to_string()
-}
-
-fn get_solution(month: Point, day: Point) -> Vec<Board> {
-    let board = Board::new_from_day_pos(month, day);
-    let blocks = Block::get_blocks();
-    let opts = SolverOptions {
-        allow_flip: false,
-        one_solution: false,
+/// month: 1~12, day: 1~31
+pub fn find_solution(month: i32, day: i32, allow_flip: bool) -> String {
+    let m = {
+        let x = if month <= 6 { 0 } else { 1 };
+        let y = (month - 1) - x * 6;
+        Point::new(x as i32, y as i32)
+    };
+    let d = {
+        let x = (day - 1) / 7 + 2;
+        let y = (day - 1) % 7;
+        Point::new(x as i32, y as i32)
     };
 
-    let sols = solve(&board, &blocks, &opts);
-    sols.into_iter().map(|s| s.board).collect()
+    let board = Board::new_from_day_pos(m, d);
+    let blocks = Block::get_blocks();
+    let opts = SolverOptions {
+        allow_flip,
+        one_solution: true,
+    };
+
+    let sols = solve(&board, &blocks, &opts)
+        .into_iter()
+        .map(|s| s.board)
+        .collect::<Vec<_>>();
+
+    if sols.is_empty() {
+        "Not found".to_string()
+    } else {
+        sols[0].to_string()
+    }
 }
