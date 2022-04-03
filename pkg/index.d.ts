ee4c71a2601aab6e4bd841b558958d7118dee6d9
keiichiw <udon.watanabe@gmail.com>
2022-04-03 17:43:48 +0000

    Deploying to gh-pages from  @ 8657de7d01820af2ac7f68b282f5366f4b6b4df5 🚀

--- pkg/index.d.ts
+++ pkg/index.d.ts
@@ -1,15 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
-* @param {number} m
-* @param {number} d
-*/
-export function greet(m: number, d: number): void;
-/**
 * month: 1~12, day: 1~31
 * @param {number} month
 * @param {number} day
+* @param {number} puzzle_type
 * @param {boolean} allow_flip
 * @returns {string}
 */
-export function find_solution(month: number, day: number, allow_flip: boolean): string;
+export function find_solution(month: number, day: number, puzzle_type: number, allow_flip: boolean): string;
--- pkg/index_bg.js
+++ pkg/index_bg.js
@@ -1,5 +1,13 @@
 import * as wasm from './index_bg.wasm';
 
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
 const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
 
 let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
@@ -18,31 +26,17 @@ function getStringFromWasm0(ptr, len) {
     return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
 /**
-* @param {number} m
-* @param {number} d
-*/
-export function greet(m, d) {
-    wasm.greet(m, d);
-}
-
-let cachegetInt32Memory0 = null;
-function getInt32Memory0() {
-    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
-        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
-    }
-    return cachegetInt32Memory0;
-}
-/**
 * month: 1~12, day: 1~31
 * @param {number} month
 * @param {number} day
+* @param {number} puzzle_type
 * @param {boolean} allow_flip
 * @returns {string}
 */
-export function find_solution(month, day, allow_flip) {
+export function find_solution(month, day, puzzle_type, allow_flip) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.find_solution(retptr, month, day, allow_flip);
+        wasm.find_solution(retptr, month, day, puzzle_type, allow_flip);
         var r0 = getInt32Memory0()[retptr / 4 + 0];
         var r1 = getInt32Memory0()[retptr / 4 + 1];
         return getStringFromWasm0(r0, r1);
@@ -51,8 +45,4 @@ export function find_solution(month, day, allow_flip) {
         wasm.__wbindgen_free(r0, r1);
     }
 }
-
-export function __wbg_alert_47c54d3b66d79e8d(arg0, arg1) {
-    alert(getStringFromWasm0(arg0, arg1));
-};
 
