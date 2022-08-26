4ee17d22b16695b938aa1a88aac68c78d7c55fcf
keiichiw <udon.watanabe@gmail.com>
2022-08-26 17:07:40 +0000

    Deploying to gh-pages from  @ dd1b25970bdad8ec9f82190bfb34c00abf06e110 🚀

---
+++ pkg/index.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* month: 1~12, day: 1~31
+* @param {number} month
+* @param {number} day
+* @param {number} puzzle_type
+* @param {boolean} allow_flip
+* @returns {string}
+*/
+export function find_solution(month: number, day: number, puzzle_type: number, allow_flip: boolean): string;
---
+++ pkg/index_bg.js
@@ -0,0 +1,48 @@
+import * as wasm from './index_bg.wasm';
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+* month: 1~12, day: 1~31
+* @param {number} month
+* @param {number} day
+* @param {number} puzzle_type
+* @param {boolean} allow_flip
+* @returns {string}
+*/
+export function find_solution(month, day, puzzle_type, allow_flip) {
+    try {
+        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+        wasm.find_solution(retptr, month, day, puzzle_type, allow_flip);
+        var r0 = getInt32Memory0()[retptr / 4 + 0];
+        var r1 = getInt32Memory0()[retptr / 4 + 1];
+        return getStringFromWasm0(r0, r1);
+    } finally {
+        wasm.__wbindgen_add_to_stack_pointer(16);
+        wasm.__wbindgen_free(r0, r1);
+    }
+}
+
